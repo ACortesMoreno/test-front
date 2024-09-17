600ab85c8301ffac6edf37d3e94e699c2cb2720b
ACortes Moreno <Lacrimadev@gmail.com>
2024-09-16 21:44:30 -0300

    First Commit

---
+++ zscore.js
@@ -0,0 +1,25 @@
+function enviarFormulario() {
+    const genero = document.getElementById('genero').value;
+    const edadMeses = document.getElementById('edadMeses').value;
+    const peso = document.getElementById('peso').value;
+    const talla = document.getElementById('talla').value;
+
+    fetch('http://localhost:3000/api/zscores', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ genero, edadMeses, peso, talla }),
+    })
+        .then(response => response.json())
+        .then(data => {
+            let resultados = '';
+            for (const [key, value] of Object.entries(data)) {
+                resultados += `${key}: ${value !== null ? JSON.stringify(value, null, 2) : 'No se encontraron valores'}\n\n`;
+            }
+            document.getElementById('resultados').innerText = resultados;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
